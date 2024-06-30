56df2ccfb724f3f70cae78ee6c2de72436aea920
Rafael Sinosaki <rafaelsinosak@gmail.com>
2024-06-30 00:37:00 -0300

    📱 responsive

--- datausa_frontend/src/app/dashboard/dashboard.component.ts
+++ datausa_frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  isSmallScreen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe((result) => {
+        this.isSmallScreen = result.matches;
+      });
+  }
+}
